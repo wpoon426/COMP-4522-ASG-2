e9d0ff7b0aff289422445a4dbe1ac34c5cbe2a77
Zaarif-Sardar <zsard040@mtroyal.ca>
2024-11-06 20:36:50 -0700

    Added employee info to sql db

--- main.py
+++ main.py
@@ -5,28 +5,55 @@ import sys
 import csv
 
 
-
 try:
     conn = mariadb.connect(
         host="localhost",
-        user="user",
+        user="zaarifsardar",
         password="",
+
     )
-    curs = conn.cursor()
-    print("Connected to Database")
+    print("Connected to mariaDB")
+   
+    cursor = conn.cursor()
+    conn.database = 'data'
+
+    employee = pandas.read_csv('data/Employee_Information.csv')
+    print(employee)
+   
+
+    for index, row in employee.iterrows():
+        cursor.execute("INSERT INTO Employee (Employee_ID, DOB, DOJ, Department_ID) values(?,?,?,?)", (row.Employee_ID, row.DOB, row.DOJ, row.Department_ID))
+    conn.commit()
+    cursor.close()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Employee (
+    Employee_ID VARCHAR(50),
+    DOB VARCHAR(20),
+    DOJ VARCHAR(20),
+    Department_ID VARCHAR(50)
+);
+""")
+    cursor 
+    
+
+
 
 except mariadb.Error as e:
     print(f"Error :(: {e}")
+
+cursor = conn.cursor()
 
 
 def load_data_to_db():
     employee = pandas.read_csv('data/Employee_Information')
     department = pandas.read_csv('data/Department_Information')
 
+    print(employee)
 
 
 def create_database():
-    curs.execute("""
+    cursor.execute("""
 CREATE TABLE IF NOT EXISTS Employee (
     Employee_ID VARCHAR(50),
     DOB VARCHAR(20),
@@ -40,11 +67,9 @@ CREATE TABLE IF NOT EXISTS Employee (
 
     
     
-    
-conn.database = ("data")
 
 
 
 
-conn.close()
+
 sys.exit()
