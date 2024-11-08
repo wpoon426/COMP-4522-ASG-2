6e6dcebf7a586c77381ab518d0d2bb82f480c4d4
Zaarif-Sardar <zsard040@mtroyal.ca>
2024-11-07 22:22:20 -0700

    Added SQL queires for data clean up, fixed DOE issue inserting intotable

--- main.py
+++ main.py
@@ -6,42 +6,42 @@ import csv
 
 
 try:
+    #Connect to db
     conn = mariadb.connect(
         host="localhost",
-        user="franklin",
+        user="zaarifsardar", #change to your username before attempting to connect
         password="",
 
 
     )
     print("Connected to mariaDB")
-
-
    
     cursor = conn.cursor()
     cursor.execute("CREATE DATABASE IF NOT EXISTS data")
     conn.database = 'data'
-
+    #get data from csv files 
     employee = pandas.read_csv('data/Employee_Information.csv')
     department = pandas.read_csv('data/Department_Information.csv')
     students = pandas.read_csv('data/Student_Counceling_Information.csv')
     performance = pandas.read_csv('data/Student_Performance_Data.csv')
 
+    #creating sql tables for each csv file
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS Employee (
     Employee_ID VARCHAR(50) PRIMARY KEY,
     DOB VARCHAR(20),
     DOJ VARCHAR(20),
     Department_ID VARCHAR(50)
-);
-""")
+        );
+        """)
     
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS Department (
     Department_ID VARCHAR(100) PRIMARY KEY,
     Department_Name LONGTEXT,
-    DOE LONGTEXT
-);
-""")
+    DOE VARCHAR(5000)
+        );
+        """)
     
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS Students (
@@ -50,8 +50,8 @@ try:
     DOB VARCHAR(20),
     Department_Choices LONGTEXT,
     Department_Admission LONGTEXT
-);
-""")
+        );
+        """)
 
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS Performance (
@@ -61,19 +61,19 @@ try:
     Paper_Name LONGTEXT,
     Marks INTEGER,
     Effort_Hours INTEGER
-);
-""")
+        );
+        """)
     
 
 
-    
+    #inserting data from csv files into sql tables
     for index, row in employee.iterrows():
         cursor.execute("INSERT INTO Employee (Employee_ID, DOB, DOJ, Department_ID) values(?,?,?,?)", (row.Employee_ID, row.DOB, row.DOJ, row.Department_ID))
     conn.commit()
 
 
     for index, row in department.iterrows():
-        cursor.execute("INSERT IGNORE INTO Department (Department_ID, Department_Name, DOE) VALUES (?,?,?)", (row.Department_ID, str(row.Department_Name), str(row.DOE)))
+        cursor.execute("INSERT IGNORE INTO Department (Department_ID, Department_Name, DOE) VALUES (?,?,?)", (row.Department_ID, str(row.Department_Name), row.DOE))
     
     conn.commit()
 
@@ -86,53 +86,12 @@ try:
         cursor.execute("INSERT INTO Performance (Student_ID, Semster_Name, Paper_ID, Paper_Name, Marks, Effort_Hours) values(?,?,?,?,?, ?)", (row.Student_ID, row.Semster_Name, row.Paper_ID, str(row.Paper_Name), row.Marks, row.Effort_Hours))
     conn.commit()
 
-    
-
-
-
-
-
-
 
 
     cursor.close()
-
-    
-    
+    sys.exit()
 
 
 
 except mariadb.Error as e:
     print(f"Error :(: {e}")
-
-cursor = conn.cursor()
-
-
-def load_data_to_db():
-    employee = pandas.read_csv('data/Employee_Information')
-    department = pandas.read_csv('data/Department_Information')
-
-    print(employee)
-
-
-def create_database():
-    cursor.execute("""
-CREATE TABLE IF NOT EXISTS Employee (
-    Employee_ID VARCHAR(50),
-    DOB VARCHAR(20),
-    DOJ VARCHAR(20),
-    Department_ID VARCHAR(50)
-);
-""")
-
-
-        
-
-    
-    
-
-
-
-
-
-sys.exit()
